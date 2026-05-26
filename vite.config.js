8f4a1c6edff682d086ea0dd4e59180d153b64c33
VAnastasiaC <v.anastasia.c@gmail.com>
2026-05-26 16:15:44 +0100

    Add vite

---
+++ vite.config.js
@@ -0,0 +1,49 @@
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
+import { defineConfig } from 'vite';
+import { dirname, resolve } from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const _dirname = dirname(fileURLToPath(import.meta.url));
+
+export default defineConfig(() => {
+    return {
+        base: './',
+        plugins: [
+            ViteImageOptimizer({
+                svg: {
+                    plugins: [
+                        'removeDoctype',
+                        'removeXMLProcInst',
+                        'minifyStyles',
+                        'sortAttrs',
+                        'sortDefsChildren',
+                    ],
+                },
+                png: {
+                    quality: 70,
+                },
+                jpeg: {
+                    quality: 70,
+                },
+                jpg: {
+                    quality: 70,
+                },
+            }),
+        ],
+        build: {
+            rollupOptions: {
+                input: {
+                    main: 'index.html',
+                    services: 'services.html',
+                    service: 'services-entry.html',
+                    publications: 'publications.html',
+                    publication: 'publications-entry.html',
+                    contact: 'contact.html',
+                    teams: 'team.html',
+                    team: 'team-entry.html',
+                    about: 'about_us.html',
+                },
+            },
+        },
+    };
+});
